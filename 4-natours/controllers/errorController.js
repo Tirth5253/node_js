8c2acb1e47b7e02b2192ccde3069db7d15e5b66d
Tirth Joshi <tirthjoshi5253@gmail.com>
2024-02-08 16:17:50 +0530

    error hand.

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,7 +1,7 @@
 //here we have put this function in the "controllers" , bcse we think handlers are also the controllers
 
 //==============================GLOBAL MIDDLEWARE FUNCTION FOR OPERATIONAL ERROR HANDLING===============================
-                                                                             //here in this section we will wite the global middleware that will handles the all "operatinal errors" in the app           
+                                                                             //here in this section we will write the global middleware that will handles the all "operatinal errors" in the app           
 module.exports=((err,req,res,next)=>{                                               //here the first argument in the middleware is "err" means it will identify as a "error handling midd." bcse of 4 parameters, the more Error code , you will find on the "appError.js"
  
   err.statusCode=err.statusCode || 500;
